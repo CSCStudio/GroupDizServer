1a96777d88aad1c8be3113e8efc436381f5f2b33
songjiayang <songjiayang@weidaxue.me>
2014-08-02 11:40:23 +0800

    add api_controller authorize_token! function

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,2 +1,45 @@
 class  Api::ApiController < ActionController::Base
+
+  class Error < StandardError; end
+  class BadRequest < Error; end
+  class NotFound < Error; end
+  class NotAcceptable < Error; end
+  class UnprocessableEntity < Error; end
+
+
+  rescue_from BadRequest do |e|
+    head :bad_request
+  end
+
+  rescue_from NotFound do |e|
+    head :not_found
+  end
+
+  rescue_from NotAcceptable do |e|
+    head :not_acceptable
+  end
+
+  rescue_from UnprocessableEntity do |e|
+    head :unprocessable_entity
+  end
+
+  before_action :authorize_token!
+
+  protected
+
+  def authorize_token!
+    auth_token = nil
+    auth_header =  request.headers['Authorization']
+    if auth_header.nil?
+      auth_token = params[:token]
+    else
+      match = auth_header.match(/\AGROUP\s+.*?token=("|')(.+?)\1.*?\z/)
+      auth_token = match[2] unless match.nil?
+    end
+    raise NotAcceptable if auth_token.blank?
+
+    current_user = User.where(:identifier => auth_token).first
+
+    raise UnprocessableEntity if current_user.nil?
+  end
 end
